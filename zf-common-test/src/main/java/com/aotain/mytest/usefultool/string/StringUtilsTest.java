15336a268f282dd6f8d496a21fb3914b9ce6d395
bang <daiyuhang417@gmail.com>
2018-04-18 17:01:58 +0800

    增加常用工具类demo代码

--- zf-common-test/src/main/java/com/aotain/mytest/usefultool/string/StringUtilsTest.java
+++ zf-common-test/src/main/java/com/aotain/mytest/usefultool/string/StringUtilsTest.java
@@ -1,5 +1,10 @@
 package com.aotain.mytest.usefultool.string;
 
+import cn.hutool.core.lang.Assert;
+import org.apache.commons.lang3.StringUtils;
+import org.junit.jupiter.api.Test;
+
+
 /**
  * Demo class
  *
@@ -8,4 +13,24 @@ package com.aotain.mytest.usefultool.string;
  */
 public class StringUtilsTest {
 
+    @Test
+    public void testIsEmpty(){
+        // 检查字符串是否为空或null
+        System.out.println(StringUtils.isEmpty(null));
+        System.out.println(StringUtils.isEmpty(""));
+    }
+
+    @Test
+    public void testStrip(){
+        // 去掉字符串前后空格
+        System.out.println(StringUtils.strip("     abcd      "));
+        Assert.isTrue("abcde".equals(StringUtils.strip("     abcd      ")));
+    }
+
+    @Test
+    public void test(){
+        System.out.println(StringUtils.compareIgnoreCase("ABdc","abDc"));
+        System.out.println(StringUtils.equalsIgnoreCase("ABdc","abDc"));
+        Assert.isTrue(StringUtils.equalsIgnoreCase("ABdc","abDc"));
+    }
 }
